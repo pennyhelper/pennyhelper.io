094d9ce38b02850438a63c05f40b5820c8f40bb3
Penny Helper <65062647+pennyhelper@users.noreply.github.com>
2020-05-09 18:43:08 -0400

    changes

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-$(document).ready(function(){
-  $("a").on('click', function(event) {
-    if (this.hash !== "") {
-      event.preventDefault();
-      var hash = this.hash;
-      $('body,html').animate({
-      scrollTop: $(hash).offset().top
-      }, 1200, function(){
-      window.location.hash = hash;
-     });
-     } 
-    });
-});
-
 var width = $(window).width(); 
 
 window.onscroll = function(){
